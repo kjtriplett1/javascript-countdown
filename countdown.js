e75c0e11ade3087ef108bf5ed8d77d9caa14abc3
kjtriplett1 <kjtriplett1@gmail.com>
2021-08-06 00:14:20 -0500

    changed the container border thickness

---
+++ countdown.js
@@ -0,0 +1,16 @@
+function doIt() {
+    setInterval(function countdown() {
+    now = new Date();
+    then = Date.parse(document.getElementById("date").value);
+    daysLeft = then - now;
+    days = Math.floor(daysLeft / (1000 * 60 * 60 * 24));
+    hours = Math.floor(daysLeft / (1000 * 60 * 60));
+    mins = Math.floor(daysLeft / (1000* 60));
+    secs = Math.floor(daysLeft / 1000);
+    dd = days;
+    hh = hours - days * 24;
+    mm = mins - hours * 60;
+    ss = secs - mins * 60;
+    document.getElementById("countdown").innerHTML = dd + ' days ' + hh + ' hours ' + mm + ' minutes ' + ss + ' seconds ';
+    },1000)   
+}
